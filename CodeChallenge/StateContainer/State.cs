0c54d93eebab16df95fbf8f49a3df69a643279b1
Hieu Le <hieuld1009@gmail.com>
2021-03-19 11:16:18 +0700

    Add project files.

---
+++ CodeChallenge/StateContainer/State.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeChallenge.StateContainer
+{
+    public class State
+    {
+        public bool request_sent { get; set; } = false;
+    }
+}
